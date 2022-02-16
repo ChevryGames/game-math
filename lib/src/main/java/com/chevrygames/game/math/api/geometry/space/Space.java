880c4c4d665ac0684d7ed169def61ac39b3034d1
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-02-16 15:09:06 -0500

    Added doc for `Space`

--- lib/src/main/java/com/chevrygames/game/math/api/geometry/space/Space.java
+++ lib/src/main/java/com/chevrygames/game/math/api/geometry/space/Space.java
@@ -1,8 +1,11 @@
 package com.chevrygames.game.math.api.geometry.space;
 
+/**
+ * A generic <a href="https://en.wikipedia.org/wiki/Space_(mathematics)">mathematical space</a> purposed for geometry.
+ */
 public interface Space {
 
-    int getDimensionality();
+    int getDimensional();
 
     boolean hasSubSpace();
 
