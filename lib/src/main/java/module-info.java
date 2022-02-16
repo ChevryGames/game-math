81339f5eb4eeaad155bc96c5c1eaf140aa1d0573
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-02-16 16:13:12 -0500

    Added TODO

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 // TODO : Setup module(s)
 // TODO : Setup SPI for the whole API https://reflectoring.io/service-provider-interface/
+// TODO : Setup workflow. See https://github.com/ChevryGames/cg-math/tree/dev/.github
 
 module game.math.api {
     requires static lombok;
