0d27d7c00abed85f6d337bfdbd4002e0881467a2
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-02-16 15:24:30 -0500

    Refactor

--- lib/src/main/java/com/chevrygames/game/math/api/geometry/space/Space.java
+++ lib/src/main/java/com/chevrygames/game/math/api/geometry/space/Space.java
@@ -6,10 +6,9 @@ package com.chevrygames.game.math.api.geometry.space;
 public interface Space {
 
     /**
-     * Gets the <a href="https://en.wikipedia.org/wiki/Dimension">dimension</a> of the <a href="https://en.wikipedia.org/wiki/Space_(mathematics)">mathematical space</a>, i.e. "the
-     * minimum number of coordinates needed to specify any point within it".
+     * Gets the <a href="https://en.wikipedia.org/wiki/Dimension">dimension</a> of the space, i.e. "the minimum number of coordinates needed to specify any point within it".
      *
-     * @return the <a href="https://en.wikipedia.org/wiki/Dimension">dimension</a> of the <a href="https://en.wikipedia.org/wiki/Space_(mathematics)">mathematical space</a>
+     * @return the <a href="https://en.wikipedia.org/wiki/Dimension">dimension</a> of the space
      */
     int getDimension();
 
